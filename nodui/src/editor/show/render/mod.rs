e9ec82744510acf0d7c05dc87be07aca084e787b
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2024-12-10 12:10:10 +0100

    🐛 fix node size

--- nodui/src/editor/show/render/mod.rs
+++ nodui/src/editor/show/render/mod.rs
@@ -10,5 +10,8 @@ const SOCKET_WIDTH: f32 = 10.0;
 /// Space between socket's name its socket shape.
 const SOCKET_NAME_GAP: f32 = 5.0;
 
+/// Space between the two columns of a node.
+const DOUBLE_COLUMNS_GAP: f32 = 5.0;
+
 /// The height of a socket line.
 const ROW_HEIGHT: f32 = 20.0;
